2355ee7fa1054afeee01b1cf09063439be4321bd
Mattia-Giorgetti <mattiagiorgetti37@gmail.com>
2023-01-23 18:16:56 +0100

    prova chiamata api

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,11 @@
 <template>
   <div>
-
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
-  export default {
-    
-  }
+export default {};
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
