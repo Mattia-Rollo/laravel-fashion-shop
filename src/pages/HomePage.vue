26d8f90f72131bb7f49b249babb828d35cd46f4e
Mattia-Giorgetti <mattiagiorgetti37@gmail.com>
2023-01-23 18:17:41 +0100

    api call tryout

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,15 +1,28 @@
 <template>
-    <div>
-
-    </div>
+  <div></div>
 </template>
 
 <script>
-    export default {
-        
-    }
+import { store } from "../store";
+import axios from "axios";
+export default {
+  name: "HomePage",
+  data() {
+    return {
+      store,
+    };
+  },
+  methods: {
+    callProducts() {
+      axios.get(`${this.store.apiBaseUrl}/products`).then((res) => {
+        console.log(res.data.results);
+      });
+    },
+  },
+  mounted() {
+    this.callProducts();
+  },
+};
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
