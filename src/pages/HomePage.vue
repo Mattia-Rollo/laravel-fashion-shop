2e8e71c42f8187e67c74cc1fd26389270c2c2de0
StefaniaSgreva <stefaniasgreva@gmail.com>
2023-01-24 12:48:33 +0100

    addd general scss rules

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,5 +1,18 @@
 <template>
-  Home
+  <div class="mk_container">
+    Home
+    <button class="mk_btn">Browse Products</button>
+    <h1>Titolo</h1>
+    <h2>Titolo 2</h2>
+    <h3>Made Only From <br>Organic Materials</h3>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Molestiae architecto corporis atque distinctio facere, eveniet alias quasi repudiandae quia quae! Sapiente minima reiciendis ab itaque corrupti, non deserunt laborum soluta!</p>
+    <ul>
+      <li><a href="">Prova</a></li>
+      <li><a href="">Prova</a></li>
+      <li><a href="">Prova</a></li>
+      <li><a href="">Prova</a></li>
+    </ul>
+  </div>
 </template>
 
 <script>
