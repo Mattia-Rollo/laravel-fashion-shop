03317cb4ef3749af42cb992dd8515749d1fdc78e
StefaniaSgreva <stefaniasgreva@gmail.com>
2023-01-24 18:19:19 +0100

    added home hero plus first two sections

---
+++ src/components/HomeHeroComponent.vue
@@ -0,0 +1,84 @@
+<template>
+    <section id="hero">
+        <div class="home_hero_img"></div>
+        <div class="home_hero_title">
+            <h1>Less Shadow, More Glow</h1>
+        </div>
+        <div class="home_hero_bg"></div>
+
+        <div class="home_hero_highlight mk_container">
+
+            <div class="home_hero_highlight_img">
+            </div>
+            <div class="infos">
+                <h3>Made Only From<br>Organic Materials</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                <button class="mk_btn">Browse Products</button>
+            </div>
+
+        </div>
+
+    </section>
+</template>
+
+<script>
+    export default {
+        name: 'HomeHeroComponent',
+    }
+</script>
+
+<style lang="scss" scoped>
+@use '../assets/styles/partials/variables' as *;
+@use '../assets/styles/partials/mixins' as *;
+
+.home_hero_img{
+    height: 550px;
+    background-image: url(../assets/img/temp_hero.PNG);
+    background-repeat: no-repeat;
+    background-size: cover;
+    background-position: center;
+}
+.home_hero_title{
+  
+    text-align: center;
+    background: $mk_bg_pink;
+    width: 1000px;
+    margin: 0 auto;
+    border-radius: 15px;
+    margin-top: -50px;
+}
+.home_hero_bg{
+    height: 400px;
+    background: $mk_bg_pink;
+    margin-top: -52px;
+}
+.home_hero_highlight{
+    // max-width: 1138px;
+    margin-top: -300px;
+    height: 590px;
+    @include mk_dflex_between;
+    margin-bottom: 5rem;
+
+
+    .home_hero_highlight_img{
+        height: 100%;
+        width: 35%;
+        background-image: url(../assets/img/organic_materials.jpg);
+         background-size: cover;
+         background-repeat: no-repeat;
+        background-position: center;
+    }
+    .infos{
+        height: 100%;
+        width: 65%;
+        background-color: $mk_bg_mint;
+        @include mk_dflex_start;
+        padding: 0 8rem;
+
+        p{
+            margin: 1rem 0 3rem 0;
+        }
+    }
+}
+
+</style>
