688fba37cc18f0d53c0b4b8fc29cac1847edcb1c
Mattia-Giorgetti <mattiagiorgetti37@gmail.com>
2023-01-25 17:31:28 +0100

    contact page add

---
+++ src/components/ContactsFormComponent.vue
@@ -0,0 +1,104 @@
+<template>
+  <form action="">
+    <div class="form_inputs">
+      <div class="position-relative">
+        <input type="text" id="name" name="name" />
+        <label for="name">Name</label>
+      </div>
+      <div class="position-relative">
+        <input type="email" id="email" name="email" />
+        <label for="email">Email Address</label>
+      </div>
+    </div>
+    <div class="form_textarea">
+      <div class="position position-relative">
+        <textarea name="message" id="message"></textarea>
+        <label for="message">Write a message</label>
+      </div>
+    </div>
+    <button class="mk_btn">Submit</button>
+  </form>
+</template>
+
+<script>
+import axios from "axios";
+import { store } from "../store";
+export default {
+  name: "ContactsFormComponent",
+  data() {
+    return {
+      store,
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@use "../assets/styles/partials/variables" as *;
+@use "../assets/styles/partials/mixins" as *;
+
+form {
+  padding: 5rem;
+  background-color: $mk_bg_mint;
+}
+.form_inputs {
+  display: flex;
+  justify-content: space-between;
+  input {
+    width: 500px;
+    padding: 1.8rem;
+    outline: none;
+    border-radius: 10px;
+    border: 2px solid $mk_white;
+    background: transparent;
+  }
+  label {
+    position: absolute;
+    top: 50%;
+    transform: translateY(-50%);
+    left: 2rem;
+    font-size: 1.2rem;
+    color: $mk_white;
+    transition: top 0.3s ease;
+    padding: 0.4rem;
+  }
+  input:focus ~ label {
+    top: 0;
+    background-color: $mk_bg_mint;
+  }
+}
+.form_textarea {
+  margin-top: 3rem;
+
+  textarea {
+    width: 100%;
+    height: 400px;
+    background-color: transparent;
+    outline: none;
+    border: 2px solid $mk_white;
+    border-radius: 10px;
+    padding: 2rem;
+    resize: none;
+  }
+  label {
+    position: absolute;
+    top: 1.5rem;
+    left: 2rem;
+    font-size: 1.2rem;
+    color: $mk_white;
+    padding: 0.4rem;
+    transition: top 0.3s ease;
+  }
+  textarea:focus ~ label {
+    top: -1.3rem;
+    background-color: $mk_bg_mint;
+  }
+}
+button.mk_btn {
+  margin-top: 2rem;
+  display: block;
+  margin-left: auto;
+  //   width: fit-content;
+  //   padding: 0.5rem 1.6rem;
+}
+</style>
--- src/pages/ContactUs.vue
+++ src/pages/ContactUs.vue
@@ -1,11 +1,65 @@
 <template>
-  <div>Pagina dei contatti</div>
+  <section id="contacts">
+    <div class="mk_container">
+      <h2 class="text-center">Drop Us a Message</h2>
+      <div class="contact_form">
+        <ContactsFormComponent />
+      </div>
+      <div class="contact_info">
+        <div class="contact_info_inner">
+          <h5>Location</h5>
+          <p class="mb-0">22 Div Mountain,</p>
+          <p>San Francisco, California</p>
+        </div>
+        <div class="contact_info_inner">
+          <h5>Phone</h5>
+          <p class="text-decoration-underline">+8801740376995</p>
+        </div>
+        <div class="contact_info_inner">
+          <h5>Email</h5>
+          <p class="text-decoration-underline">mail@domain.com</p>
+        </div>
+        <div class="contact_info_inner">
+          <h5>Hours</h5>
+          <p>Everyday: 8am-5pm</p>
+        </div>
+      </div>
+      <h2 class="text-center">Subscribe Our Newsletter</h2>
+    </div>
+  </section>
 </template>
 
 <script>
+import ContactsFormComponent from "../components/ContactsFormComponent.vue";
+import { store } from "../store";
 export default {
   name: "ContactUs",
+  data() {
+    return {
+      store,
+    };
+  },
+  components: { ContactsFormComponent },
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+@use "../assets/styles/partials/variables" as *;
+@use "../assets/styles/partials/mixins" as *;
+
+h2 {
+  padding: 2rem 0;
+}
+h5 {
+  font-size: 1.5rem;
+  margin-bottom: 1rem;
+}
+.contact_info {
+  display: flex;
+  justify-content: space-between;
+  margin-top: 4rem;
+}
+.text-decoration-underline:hover {
+  cursor: pointer;
+}
+</style>
