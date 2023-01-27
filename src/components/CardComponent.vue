831dcd4e608999b550e77015f8f36c049ff9e706
StefaniaSgreva <stefaniasgreva@gmail.com>
2023-01-27 19:51:29 +0100

    about us images

--- src/components/CardComponent.vue
+++ src/components/CardComponent.vue
@@ -100,53 +100,5 @@ export default {
 
   }
 }
-// .mk_col {
-//   width: calc(100% / 3);
-// }
-// .img_wrap {
-//   width: 200px;
-//   height: 200px;
-//   margin: 0 auto;
-//   padding-bottom: 1rem;
-//   overflow: hidden;
 
-//   & img {
-//     object-fit: cover;
-//   }
-// }
-// .mk_card {
-//   padding: 1rem;
-//   background-color: $mk_white;
-//   box-shadow: 0px 4px 8px -2px rgba($mk_txt_dark_gray, 0.35);
-// }
-// .card_colors_section {
-//   display: flex;
-//   align-items: center;
-//   gap: 0.5rem;
-//   margin-top: 1rem;
-//   padding: 1rem 2rem;
-//   .hex_dot {
-//     width: 40px;
-//     height: 40px;
-//     background-color: $mk_bg_pink;
-//     border-radius: 50%;
-//   }
-// }
-// .card_content {
-//   h5 {
-//     font-size: 1.5rem;
-//     font-weight: 700;
-//   }
-//   p {
-//     font-size: 1.2rem;
-//   }
-//   .price_section {
-//     font-size: 1.3rem;
-//     font-weight: bold;
-
-//     span {
-//       padding-right: 0.5rem;
-//     }
-//   }
-// }
 </style>
