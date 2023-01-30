fb6aa63a766d4edc5a11d2cb826ee4fd7f7b7029
Mattia-Fasulo <mattia.fasulo@gmail.com>
2023-01-30 15:13:09 +0100

    fix not-found

--- src/pages/NotFound.vue
+++ src/pages/NotFound.vue
@@ -1,5 +1,14 @@
 <template>
-  <h2 class="text-center text-uppercase py-5">Page not found</h2>
+  
+  <div class="m-auto">
+    <div style="width: 100%; height: 0%; padding-bottom: 56%; position: relative">
+      <iframe src="https://giphy.com/embed/g01ZnwAUvutuK8GIQn" width="100%" height="100%" style="position: absolute"
+        frameBorder="0" class="giphy-embed" allowFullScreen></iframe>
+    </div>
+    <p>
+      <a href="https://giphy.com/gifs/high-quality-highqualitygifs-g01ZnwAUvutuK8GIQn"></a>
+    </p>
+  </div>
 </template>
 
 <script>
