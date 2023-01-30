7bf3963b5cd5c43ec57324569a8a86f5fe799b5c
Mattia-Fasulo <mattia.fasulo@gmail.com>
2023-01-30 13:17:26 +0100

    fix cart

--- src/components/ShoppingCartComponent.vue
+++ src/components/ShoppingCartComponent.vue
@@ -6,20 +6,15 @@
       </div>
       <h6>Shopping Cart</h6>
       <div class="product_list mt-5">
-        <div
-          class="cart_item d-flex py-3 justify-content-between align-items-center"
-          v-for="(cartItem, index) in store.shoppingCart"
-          :key="index"
-        >
+        <div class="cart_item d-flex py-3 justify-content-between align-items-center"
+          v-for="(cartItem, index) in store.shoppingCart" :key="index">
           <div>
             <p>{{ cartItem.name }}</p>
             <p>{{ cartItem.price_sign }}{{ cartItem.price }}</p>
-            <input type="number" class="w-25 me-2" @change="" />
+            <input type="number" class="w-25 me-2" v-model="cartItem.quantity" min="1"/>
             <label for="quantity">Quantity</label>
           </div>
-          <span @click="removeFromCart()"
-            ><i class="fa-solid fa-trash"></i
-          ></span>
+          <span @click="removeFromCart()"><i class="fa-solid fa-trash"></i></span>
         </div>
       </div>
       <div class="cart_total mt-4 d-flex justify-content-between me-auto">
@@ -36,8 +31,7 @@ export default {
   name: "CartComponent",
   data() {
     return {
-      store,
-      arrayquantity: [],
+      store  
     };
   },
   methods: {
@@ -50,19 +44,25 @@ export default {
       return store.shoppingCart.reduce((a, b) => a + b.price * b.quantity, 0);
     },
   },
+  mounted() {
+    
+  }
 };
 </script>
 
 <style lang="scss" scoped>
 @use "../assets/styles/partials/variables" as *;
 @use "../assets/styles/partials/mixins" as *;
+
 .product_list {
   overflow-y: auto;
   height: 70vh;
 }
+
 .cart_item {
   border-bottom: 1px solid black;
 }
+
 .shopping_cart {
   position: fixed;
   top: 0;
